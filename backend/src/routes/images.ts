7b1645ed9b2c90b6147c14cc349988a6ce7b1f06
Lev Haikin <levhaikin@gmail.com>
2025-06-10 11:29:26 +0300

    * add authentication for images api

--- backend/src/routes/images.ts
+++ backend/src/routes/images.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { getWordImage } from '../controllers/imageController';
+import { authenticateToken } from '../middleware/authMiddleware';
+
 
 const router = Router();
 
+// router.get('/word/:word', authenticateToken, getWordImage);
 router.get('/word/:word', getWordImage);
 
 export default router; 
